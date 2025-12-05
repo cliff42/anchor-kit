b36399b57d755c9cf94df27f3d5c2acfd2d5cd47
chris cliff <cliff42@students.cs.ubc.ca>
2025-12-05 01:54:35 -0500

    add measure pass and rework layout pass

--- crates/anchor-kit-core/src/layout.rs
+++ crates/anchor-kit-core/src/layout.rs
@@ -17,7 +17,7 @@ fn handle_element_layout(
     allocated_origin: [u32; 2],
     allocated_size: [u32; 2],
 ) {
-    match &element._type {
+    match element._type.clone() {
         ElementType::Root => {
             // TODO: should we panic here? or bubble up an error instead? (should probably have a check to make sure the tree is valid by not having 2 roots)
             for c in element.children.iter_mut() {
@@ -25,12 +25,12 @@ fn handle_element_layout(
             }
         }
         ElementType::Anchor(anchor_position) => {
-            handle_anchor_element(element, *anchor_position, allocated_origin, allocated_size);
+            handle_anchor_element(element, anchor_position, allocated_origin, allocated_size);
         }
         ElementType::Text(_) => {
-            handle_text_element(element, allocated_origin, allocated_size);
+            handle_text_element(element, allocated_origin);
         }
-        ElementType::FlexRow => handle_flex_row(element, allocated_origin, allocated_size),
+        ElementType::FlexRow => handle_flex_row(element, allocated_origin),
     }
 }
 
@@ -41,7 +41,7 @@ fn handle_anchor_element(
     allocated_size: [u32; 2],
 ) {
     let [aw, ah] = allocated_size;
-    let [ew, eh] = element.size;
+    let [ew, eh] = element.size; // set in measure pass
 
     // produces the relative x,y that all children elements should be anchored to for rendering
     let (rel_x, rel_y) = match anchor_position {
@@ -65,61 +65,34 @@ fn handle_anchor_element(
         handle_element_layout(
             c,
             element.frame_position.unwrap_or(FRAME_ORIGIN),
-            element.size,
+            element.size, // TODO: do we need to account for padding on the anchor position here?
         );
     }
 }
 
-fn handle_text_element(
-    element: &mut Element,
-    allocated_origin: [u32; 2],
-    allocated_size: [u32; 2],
-) {
-    let text = match &element._type {
-        ElementType::Text(s) => s.as_str(),
-        _ => return,
-    };
-
-    // TODO: these are placeholder values for now, should be dictated by styling
-    let char_w: u32 = 8;
-    let line_h: u32 = 16;
-
-    let text_width = (text.chars().count() as u32).saturating_mul(char_w);
-    let text_height = line_h;
-
-    let [aw, ah] = allocated_size;
-    let text_box_w = text_width.min(aw);
-    let text_box_h = text_height.min(ah);
-
+fn handle_text_element(element: &mut Element, allocated_origin: [u32; 2]) {
     element.frame_position = Some(allocated_origin);
-    element.size = [text_box_w, text_box_h];
 }
 
-fn handle_flex_row(element: &mut Element, allocated_origin: [u32; 2], allocated_size: [u32; 2]) {
-    let num_children = element.children.len();
-
-    let padding: u32 = 8; // TODO: this is a placeholder for now, it should be set by styling
-
-    // for flex elements we want to split the allocated space into even sizes for each of the child elements
-    let [aw, ah] = allocated_size;
-    let total_padding = if num_children > 1 {
-        (num_children - 1) as u32 * padding
-    } else {
-        0
-    };
-    let available_width_after_padding = aw.saturating_sub(total_padding as u32);
-    let width_per_child = if num_children > 0 {
-        available_width_after_padding / num_children as u32
-    } else {
-        aw
-    };
-    let height_per_child = ah; // flex row only handles flex in width direction
+fn handle_flex_row(element: &mut Element, allocated_origin: [u32; 2]) {
+    let padding_between_children: u32 = 8; // TODO: this is a placeholder for now, it should be set by styling
 
     let [ax, ay] = allocated_origin;
+    element.frame_position = Some(allocated_origin);
+
+    let mut x_offset = ax; // current offset of where to place the next child
 
     // left to right rendering order is assumed for now, but should be configurable in the future
     for (i, c) in element.children.iter_mut().enumerate() {
-        let curr_child_origin = [ax + (width_per_child + padding) * i as u32, ay];
-        handle_element_layout(c, curr_child_origin, [width_per_child, height_per_child]);
+        let [cw, ch] = c.size; // this is already set properly by the measure pass
+
+        if i > 0 {
+            x_offset = x_offset.saturating_add(padding_between_children);
+        }
+
+        let curr_child_origin = [x_offset, ay];
+        handle_element_layout(c, curr_child_origin, [cw, ch]);
+
+        x_offset = x_offset.saturating_add(cw); // add the current child's width so the next child is offset correctly
     }
 }
